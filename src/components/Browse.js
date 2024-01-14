3983e5c6164e405b49ff93b040336b14e4b6b507
090Pratik <ppd731@gmail.com>
2024-01-14 23:37:37 +0530

    gptsearch commented

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,29 +1,10 @@
-// import Header from "./Header";
-// import MainContainer from "./MainContainer";
-// import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
-// import SecondaryContainer from "./SecondaryContainer";
-
-// const Browse = () => {
-//   useNowPlayingMovies();
-
-//   return (
-//     <div>
-//       <Header />
-//       <MainContainer />
-//       <SecondaryContainer />
-//     </div>
-//   );
-// };
-
-// export default Browse;
-
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useUpcomingMovies from "../hooks/useUpcomingMovies";
-import GptSearch from "./GptSearch";
+//import GptSearch from "./GptSearch";
 import { useSelector } from "react-redux";
 
 const Browse = () => {
